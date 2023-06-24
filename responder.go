3c1ab02500438cf48dfb2bab50ecadc1975b2254
Ali <sr.ali.torabi@gmail.com>
2023-06-24 22:41:04 +0330

    add new response struct + total items count to responses

--- responder.go
+++ responder.go
@@ -3,7 +3,15 @@ package handlerlib
 import "github.com/aliworkshop/errorslib"
 
 type Responder interface {
+	SetTotal(uint) Responder
 	Respond(RequestModel, Status, interface{})
 	RespondWithError(model RequestModel, err errorslib.ErrorModel)
 	LanguageHandler
+}
+
+type Response struct {
+	Page    int `json:"page"`
+	PerPage int `json:"per_page"`
+	Items   any  `json:"items"`
+	Total   uint `json:"total"`
 }
