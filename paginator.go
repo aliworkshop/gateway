28f2d5cb92d5021388feeea62ad8701135afac07
Ali <sralitorabi@gmail.com>
2026-05-08 13:53:16 +0400

    refactor paginator interface to use int instead of int32 for page size and page

--- paginator.go
+++ paginator.go
@@ -1,21 +1,21 @@
 package gateway
 
 type Paginator interface {
-	GetPageSize() int32
-	GetPage() int32
+	GetPageSize() int
+	GetPage() int
 	Total() uint64
 }
 
 type IPaginator interface {
 	Paginator
-	SetPage(int32)
-	SetPageSize(int32)
+	SetPage(int)
+	SetPageSize(int)
 	SetTotal(uint64)
 }
 
 type paginator struct {
-	limit int32
-	page  int32
+	limit int
+	page  int
 	total uint64
 }
 
@@ -23,22 +23,22 @@ func NewPaginator() IPaginator {
 	return &paginator{}
 }
 
-func (p *paginator) SetPageSize(limit int32) {
+func (p *paginator) SetPageSize(limit int) {
 	p.limit = limit
 }
 
-func (p *paginator) SetPage(page int32) {
+func (p *paginator) SetPage(page int) {
 	p.page = page
 }
 
-func (p *paginator) GetPageSize() int32 {
+func (p *paginator) GetPageSize() int {
 	if p.limit == 0 {
 		p.limit = 10
 	}
 	return p.limit
 }
 
-func (p *paginator) GetPage() int32 {
+func (p *paginator) GetPage() int {
 	if p.page <= 0 {
 		p.page = 1
 	}
